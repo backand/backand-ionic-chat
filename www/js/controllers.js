d92996a9eb80638ec493218f4bcdca320bdcfa4b
Tamir Kfir <tamir.kk@gmail.com>
2016-09-02 11:14:37 +0300

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,24 @@
+angular.module('starter')
+
+
+  .controller('ChatsCtrl', function (Chats, Backand) {
+    var self = this;
+    Chats.getAll().then(function (response) {
+      self.chats = response.data.data;
+    });
+  })
+
+  .controller('ChatDetailCtrl', function ($stateParams, Chats, Backand, Messages) {
+    var self = this;
+    self.messages = [];
+    Backand.on('send_message' + $stateParams.chatId, function (data) {
+      self.messages.push(data[1].Value);
+    });
+    Chats.get($stateParams.chatId).then(function (response) {
+      self.chat = response.data;
+    });
+    self.sendMessage = function () {
+      Messages.post(self.chat.newMessage, $stateParams.chatId);
+      self.chat.newMessage = '';
+    };
+  });
